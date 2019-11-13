faf05d8727c7a0b337df21f14683e7753ba552cb
Anderson Hicher <hicherhp@gmail.com>
2019-11-13 16:21:30 -0300

    Adding the commentaries at the code;

--- src/Veterinario.java
+++ src/Veterinario.java
@@ -45,34 +45,45 @@ public class Veterinario {
 	
 	//Cadastro de Veterinario:
 	public void cadastroVeterinario(Veterinario veterinario) throws IOException{
+		
 		//Criando o ArrayList:
 		ArrayList<Object> veterinarios = new ArrayList<>();
+		
 		//Abrindo arquivo onde será salva a lista de veterinários:
 		FileWriter file = new FileWriter("ListaDeVeterinarios.txt", true);
 		PrintWriter writer = new PrintWriter(file);
+		
 		//Adicionando Objeto à lista de cadastro:
 		veterinarios.add(veterinario.getCrmv()+";"+veterinario.getNome()+";"+veterinario.getEspecialidade());
+		
 		//Cadastrando Vetor de veterinários:
 		writer.println(veterinarios);
+		
 		//Fechando Arquivo de veterinários:
 		file.close();
 	}
 	
 	//Listar veterinários cadastrados:
 	public ArrayList<String> listarVeterinarios() throws IOException{
+		
+		//Abrindo arquivo para leitura:
 		FileReader file = new FileReader("ListaDeVeterinarios.txt");
 		BufferedReader reader = new BufferedReader(file);
 		
+		//Criando ArrayList para manter salvo os dados do arquivo:
 		ArrayList<String> veterinarios = new ArrayList<>();
 		
+		//Lendo dados do arquivo e salvando-os na ArrayList:
 		String linha =reader.readLine();
 		while(linha != null){
             veterinarios.add(linha);
             linha = reader.readLine();
         }
         
-        
+        //Fechando arquivo utilizado:
 		file.close();
+		
+		//Retornando o ArrayList preenchido;
 		return veterinarios;
 	
 	}
@@ -80,22 +91,37 @@ public class Veterinario {
 	//Buscar Veterinario Cadastrado pelo Nome:
 	
 	public String buscaNome(String nome) throws IOException{
+		
+		//Cria um ArrayList e o preenche com o retorno do método listarVeterinarios():
 		ArrayList<String> veterinarios= new ArrayList<>();
 		veterinarios = listarVeterinarios();
+		
+		//Captura o tamanho do ArrayList criado:
 		int total = veterinarios.size();
+		
+		//Cria um array simples para armazenar os dados de um único veterinário cadastrado:
 		String arrayVeterinario[] = new String[3]; 
 		int flag = 0;
 		while(flag < total) {
+			
+			//Percorre o ArrayList e armazena os dados do veterinário atual 
 			String veterinario = veterinarios.get(flag);
+			
+			//Quebra a string de dados
 			arrayVeterinario = veterinario.split(";");
+			
+			//Verifica se o nome buscado é igual ao nome cadastrado
 			String nomeCarregado = arrayVeterinario[1];
 			if( nomeCarregado.equals(nome)) {
+				
+				//Se o nome verificado for igual, retorna toda a string de dados
 				return veterinario;
 			}else {
 				flag++;
 			}
 		}
 		
+		//Se o nome não for encontrado retorna nulo:
 		return null;
 	}
 }
