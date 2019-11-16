c1c802016380df071319464aff37b70b8a896772
Anderson Hicher <hicherhp@gmail.com>
2019-11-16 05:50:45 -0300

    Starting the test battery of the Paciente class;

--- src/MenuInicial.java
+++ src/MenuInicial.java
@@ -10,8 +10,57 @@ import java.io.IOException;
 public class MenuInicial {
 
 	public static void main(String[] args) {
+		Paciente teste = new Paciente("Snow", "Masculino", "Akita", 30, 1, "13113319641", "16/11/2019");
+		/*
+		//Teste de cadastro:
+		try {
+			System.out.println("Entrou no cadastro:");
+			teste.cadastroPaciente(teste);
+		} catch (Exception e) {
+			System.out.println("Falha ao entrar no cadastro: ");
+			System.out.println(e.getMessage());
+		}
 		
 		
+		//Teste de listagem de pacientes:
+		try {
+			System.out.println("Entrou no método de listagem:");
+			teste.listarPacientes();
+		} catch (Exception e) {
+			System.out.println("Falha ao listar ");
+			System.out.println(e.getMessage());
+		}
+			
+			
+		//Teste de busca:
+		try {
+			System.out.println("Entrou na busca:");
+			teste.buscaId(0);
+		} catch (Exception e) {
+			System.out.println("Falha ao entrar na busca: ");
+			System.out.println(e.getMessage());
+		}
+			
+		//Teste de edição:
+		try {
+			System.out.println("Entrou na edição:");
+			teste.editarPacienteCadastrado(0, "kira", "feminino", "Fila", 60, 3, "35930926620", "16/11/2018");
+		} catch (Exception e) {
+			System.out.println("Falha ao entrar na edição: ");
+			System.out.println(e.getMessage());
+		}
+		
+		//Teste de exclusão:
+		try {
+			System.out.println("Entrou na exclusão:");
+			teste.excluirPacienteCadastrado(1);
+		} catch (Exception e) {
+			System.out.println("Falha ao entrar na exclusão: ");
+			System.out.println(e.getMessage());
+		}
+		
+		
+		*/
 	} 
 
 }
