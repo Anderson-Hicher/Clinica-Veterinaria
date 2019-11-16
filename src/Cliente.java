78620dfe9972f1bfc6ba0259ce6229e2378d46ca
Anderson Hicher <hicherhp@gmail.com>
2019-11-16 13:02:53 -0300

    Adding the cadastroClientes() and copying the method listarClientes() from the class Veterinario and adapt to the Cliente class

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,3 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 /**
  * @author andersonhicher
  *
@@ -58,5 +65,54 @@ public class Cliente {
 	/*
 	 * ################### Cadastro de cliente: ###################
 	 */
+	
+	public void cadastroCliente(Cliente cliente) throws IOException{
+		
+		//Criando o ArrayList:
+		ArrayList<Object> clientes = new ArrayList<>();
+		//Abrindo arquivo onde será salva a lista de clientes:
+		FileWriter file = new FileWriter("ListaDeClientes.txt", true);
+		PrintWriter writer = new PrintWriter(file);
+		
+		//Adicionando Objeto à lista de cadastro:
+		clientes.add(cliente.getCpf()+";"+cliente.getNome()+";"+cliente.getEndereco()+";"+cliente.getTelefone());
+		
+		//Cadastrando Vetor de clientes:
+		writer.println(clientes);
+		
+		//Fechando Arquivo de clientes:
+		file.close();
+	}
+	
+	/*
+	 * #################### Listar clientes cadastrados: #################
+	 */
+	
+	public ArrayList<String> listarVeterinarios() throws IOException{
+		
+		//Abrindo arquivo para leitura:
+		FileReader file = new FileReader("ListaDeVeterinarios.txt");
+		BufferedReader reader = new BufferedReader(file);
+		
+		//Criando ArrayList para manter salvo os dados do arquivo:
+		ArrayList<String> veterinarios = new ArrayList<>();
+		
+		//Lendo dados do arquivo e salvando-os na ArrayList:
+		String vetor =reader.readLine();
+		while(vetor != null){
+			String linha = vetor.substring(1, vetor.length()-1);
+            veterinarios.add(linha);
+            vetor = reader.readLine();
+        }
+        
+        //Fechando arquivo utilizado:
+		file.close();
+		
+		//Retornando o ArrayList preenchido;
+		
+		return veterinarios;
+	
+	}
+	
 	
 }
