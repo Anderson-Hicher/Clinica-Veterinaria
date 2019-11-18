5a8f5cc034b6012c9ecaff531b799a1378fd6537
Anderson Hicher <hicherhp@gmail.com>
2019-11-18 01:13:48 -0300

    Adding the ListaDeVeterinarios.txt and the MenuInicial.java at the program package

--- src/MenuInicial.java
+++ src/MenuInicial.java
@@ -10,57 +10,61 @@ import java.io.IOException;
 public class MenuInicial {
 
 	public static void main(String[] args) {
-		Paciente teste = new Paciente("Snow", "Masculino", "Akita", 30, 1, "13113319641", "16/11/2019");
-		/*
+		Paciente teste = new Paciente("Snow", "Masculino", "Akita", 30, 1, "13113319641", "14/11/2019", "24/04/1997");
+		
+		
+		
 		//Teste de cadastro:
 		try {
 			System.out.println("Entrou no cadastro:");
 			teste.cadastroPaciente(teste);
-		} catch (Exception e) {
+		} catch (IOException e) {
 			System.out.println("Falha ao entrar no cadastro: ");
 			System.out.println(e.getMessage());
 		}
 		
 		
-		//Teste de listagem de pacientes:
+		//Teste de listagem :
 		try {
 			System.out.println("Entrou no método de listagem:");
-			teste.listarPacientes();
-		} catch (Exception e) {
+			System.out.println(teste.listarPacientes());
+		} catch (IOException e) {
 			System.out.println("Falha ao listar ");
 			System.out.println(e.getMessage());
 		}
-			
-			
+		
+		
 		//Teste de busca:
 		try {
 			System.out.println("Entrou na busca:");
-			teste.buscaId(0);
-		} catch (Exception e) {
+			System.out.println(teste.buscaId("S52"));
+		} catch (IOException e) {
 			System.out.println("Falha ao entrar na busca: ");
 			System.out.println(e.getMessage());
 		}
-			
+		
+		
 		//Teste de edição:
 		try {
 			System.out.println("Entrou na edição:");
-			teste.editarPacienteCadastrado(0, "kira", "feminino", "Fila", 60, 3, "35930926620", "16/11/2018");
-		} catch (Exception e) {
+			teste.editarPacienteCadastrado("S52", "Kira", "Femea", "Fila", 75, 6, "35930926620", "14/11/2019", "22/06/2010");
+		} catch (IOException e) {
 			System.out.println("Falha ao entrar na edição: ");
 			System.out.println(e.getMessage());
 		}
 		
+		
 		//Teste de exclusão:
 		try {
 			System.out.println("Entrou na exclusão:");
-			teste.excluirPacienteCadastrado(1);
-		} catch (Exception e) {
+			teste.excluirPacienteCadastrado("K80");
+		} catch (IOException e) {
 			System.out.println("Falha ao entrar na exclusão: ");
 			System.out.println(e.getMessage());
 		}
 		
 		
-		*/
+		
 	} 
 
 }
